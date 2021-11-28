3fd62729b6a1963a3bf5fc021fb9e1113bc88c24
simmsb3 <brendan.simms3@mail.dcu.ie>
2021-11-28 23:06:55 +0000

    Added Working links to navbar

--- javascript.js
+++ javascript.js
@@ -1,9 +1,28 @@
-var prevScrollpos = window.pageYOffset
-window.onscroll = function() {
-   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0"
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementByClass("sidebar");
+  li = ul.getElementByClass('sidebarRow');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("h4")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
     } else {
-    document.getElementById("navbar").style.top = "-50"
+      li[i].style.display = "none";
     }
-    prevScrollpos = currentScrollPos;
-  });
+  }
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementByClass("header").style.top = "0";
+  } else {
+    document.getElementByClass("header").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
